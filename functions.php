5e9b3ba60d5ae5a8737e18045609d8c4e722d1e6
GAlicea96 <galicea96@outlook.com>
2017-03-14 16:13:34 -0400

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+  $dbhost  = 'localhost';
+  $dbname  = 'SealOfHealth';
+  $dbuser  = 'admin';
+  $dbpass  = 'sealofhealth';
+
+  $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($connection->connect_error) die($connection->connect_error);
+
+  function createTable($name, $query)
+  {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+  }
+
+  function queryMysql($query)
+  {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+  
+?>
