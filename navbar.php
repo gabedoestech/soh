69acb8f8117554a5d11a7d01e8c6d6a74117402e
Gabe <gcscoding1@gmail.com>
2017-03-28 18:43:21 -0400

    navigation bar variations added

---
+++ navbar.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Seal of Health </title>
+        <meta charset="UTF-8">
+  
+
+<style type="text/css">
+/* Add a black background color to the top navigation */
+.topnav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+/*Style for logo image*/
+#logoImage
+{
+text-align:center;
+display:block;
+}
+
+img
+{
+    max-height: 150px;
+}
+
+/*Style for footer*/
+footer{
+    position:fixed;
+    bottom:0;
+    width: 100%;
+    text-align:center;
+}
+
+</style>
+</head>
+<body>
+
+<!––Title and Logo -->
+<header>
+<div id="logoImage">
+    <h1>Seal of Health</h1>
+    <img src="med1.png">
+</div>
+</header>
+
+<!––Navigation Bar -->
+<div class="topnav" id="myTopnav">
+  <a href="#home">Profile</a>
+  <a href="#news">Find Doctor</a>
+  <a href="#contact">Appointments</a>
+  <a href="#about">History</a>
+</div>
+
+<footer>
+<p>Created by Software Seals</p>
+</footer>
+
+</body>
+</html>
