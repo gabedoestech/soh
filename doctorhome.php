305f973f5c47cce23a1ab62a6b94fbd73e204d2d
Gabe <gcscoding1@gmail.com>
2017-04-20 00:12:46 -0400

    navbar and logo
    Navbar and logo changes to doctor pages.

--- doctorhome.php
+++ doctorhome.php
@@ -28,16 +28,29 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
 
 
   <head>
-    <title>Profile Information</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv-"X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+    <!-- The above 3 meta tags *must* come first in the head;
+    any other head content must come *after* these tags -->
+
+    <!--personalized CSS file by Maria -->
     <link rel="stylesheet" href="main2.css">
+    <title>Seal of Health</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!--Need the above to run dropdown menu -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     <!-- Styles nav bar -->
     <style>
@@ -137,33 +150,44 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
 
   <body>
 
-    <!-- Logo -->
-    <div class="mylogo">
-      <center><img src="Design2.png" width="608" height="230"></center>
-    </div>
+      <!-- Logo -->    
+      <div class="mylogo">
+            <center><img class="logo-img img-responsive" src="Design2.png"></center>
+      </div>
+    
 
     <!-- NEW NAVBAR -->
     <nav class="navbar navbar-default">
       <div class="container-fluid">
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <b>
-      
-<ul class="nav navbar-nav inside-full-height">
-<li><a href="#">Home</a></li>
-<li class="active"><a href="#">Profile<span class="sr-only">(current)</span></a></li>
-<li><a href="createapp.php">Appointments</a></li>
-<li><a href="help.php">Help</a></li>
-<li><a href="logout.php">Logout</a></li>
-</ul>  
-<ul class="nav navbar-right" id="log">
-<li>Logged in as: <?php echo $row2['userName']; ?></li>
-</ul>
-</div><!-- /.navbar-collapse --></b>
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
+             
+        <ul class="nav navbar-nav inside-full-height">
+        <li><a href="doctorhome.php">Home</a></li>
+
+      <!-- Dropdown for appointments -->
+              <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+                      Appointments <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="createapp.php">Create Appointment</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="scheduledapp.php">Unscheduled Appointments</a></li>
+                        <li><a href="scheduledapp.php">Scheduled Appointments</a></li>
+                        <li><a href="scheduledapp.php">Past Appointments</a></li>
+                      </ul>
+                  </li>
+      <!--End Dropdown for appointments -->
+    <li><a href="helpdoctor.php">Help</a></li>
+    </ul>  
+
+  <!--Logged in user-->
+        <ul class="nav navbar-nav navbar-right" id="log">        
+        <li><a  style="color:#03CCFE" href="#">Logged in as: <?php echo $row2['userName']; ?></a></li>
+        <li><a href="logout.php">Logout</a></li>
+        </ul>              
+      </div> <!-- /.container-fluid -->   
+    </nav> <!-- /.navbar -->
 
     <!-- OLD NAVBAR -->
     <div class="container">
@@ -231,9 +255,10 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
     </div>
 
     <center>
-      <footer class="container-fluid" id="footer">
-        <p>
-          <h4>Copyright © Software Seals, 2017.</h4></p>
+      <footer class="footer" id="footer">
+        <div class="container">
+        <h4>Copyright © Software Seals, 2017.</h4>
+      </div>
       </footer>
     </center>
   </body>
--- helpdoctor.php
+++ helpdoctor.php
@@ -27,16 +27,30 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
 
 
     <head>
-        <title>Help</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="main2.css">
+    <meta charset="utf-8">
+    <meta http-equiv-"X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+    <!-- The above 3 meta tags *must* come first in the head;
+    any other head content must come *after* these tags -->
 
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!--personalized CSS file by Maria -->
+    <link rel="stylesheet" href="main2.css">
+    <title>Seal of Health</title>
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!--Need the above to run dropdown menu -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 
         <!-- Styles nav bar -->
         <style>
@@ -127,31 +141,44 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
     </head>
 
     <body>
-        <!-- Logo -->
-        <div class="mylogo">
-            <center><img src="Design2.png" width="608" height="230"></center>
-        </div>
+       <!-- Logo -->    
+      <div class="mylogo">
+            <center><img class="logo-img img-responsive" src="Design2.png"></center>
+      </div>
+    
 
-        <!-- NEW NAVBAR -->
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
+    <!-- NEW NAVBAR -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <b><ul class="nav navbar-nav">
-        <li><a href="#">Home</a></li>
-        <li><a href="home.php">Profile</a></li>
-        <li><a href="createapp.php">Appointments</a></li>
-        <li class="active"><a href="#">Help<span class="sr-only">(current)</span></a></li>
-        <li><a href="logout.php">Logout</a></li>
-        </ul>
+             
+        <ul class="nav navbar-nav inside-full-height">
+        <li><a href="doctorhome.php">Home</a></li>
 
-        <ul class="nav navbar-nav navbar-right" id="log">
-        <li>Logged in as: <?php echo $row2['userName']; ?></li>
-        </ul>
-      </div><!-- /.navbar-collapse --></b>
-      </div> <!-- /.container-fluid -->
-        </nav>
+      <!-- Dropdown for appointments -->
+              <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+                      Appointments <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="createapp.php">Create Appointment</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="scheduledapp.php">Unscheduled Appointments</a></li>
+                        <li><a href="scheduledapp.php">Scheduled Appointments</a></li>
+                        <li><a href="scheduledapp.php">Past Appointments</a></li>
+                      </ul>
+                  </li>
+      <!--End Dropdown for appointments -->
+    <li><a href="helpdoctor.php">Help</a></li>
+    </ul>  
+
+  <!--Logged in user-->
+        <ul class="nav navbar-nav navbar-right" id="log">        
+        <li><a  style="color:#03CCFE" href="#">Logged in as: <?php echo $row2['userName']; ?></a></li>
+        <li><a href="logout.php">Logout</a></li>
+        </ul>              
+      </div> <!-- /.container-fluid -->   
+    </nav> <!-- /.navbar -->
 
         <div class="container">
 
@@ -191,9 +218,13 @@ $row3 = $query2->fetch(PDO::FETCH_ASSOC);
 </div>
 
 
- <center><footer class="container-fluid" id="footer">
-    <p><h4>Copyright © Software Seals, 2017.</h4></p>
-  </footer></center>
+    <center>
+      <footer class="footer" id="footer">
+        <div class="container">
+        <h4>Copyright © Software Seals, 2017.</h4>
+      </div>
+      </footer>
+    </center>
 
 
   
